125852856c6bcbda87cea4e7daa290405109ed38
Solarex <rh.hou.work@gmail.com>
2021-12-17 11:14:45 +0800

    test

--- app/src/main/java/com/solarexsoft/practicemakesperfect/MainActivity.kt
+++ app/src/main/java/com/solarexsoft/practicemakesperfect/MainActivity.kt
@@ -2,10 +2,22 @@ package com.solarexsoft.practicemakesperfect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.solarexsoft.practicemakesperfect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.tv1.setOnClickListener {
+
+        }
     }
 }
